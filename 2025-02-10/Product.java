f8903689b90e3a426814b888ae3ed7f8484cfab2
RaulCodando <160548310+RaulCodando@users.noreply.github.com>
2025-02-11 10:28:40 -0300

    Update Product.java

--- 2025-02-10/Product.java
+++ 2025-02-10/Product.java
@@ -1,10 +1,12 @@
 public class Product{
     private String name;
     private double price;
+    private ShoppingCart shoppingCart;
 
-    public Procuct(String name, double price){
+    public Product(String name, double price){
         this.name = name;
         this.price = price;
+        shoppingCart = null;
     }
 
     public String getName(){
@@ -13,5 +15,13 @@ public class Product{
 
     public double getPrice(){
         return price;
+    }
+
+    public ShoppingCart getShoppingCart(){
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
     }
 }
